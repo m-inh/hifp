58bdfde9c30d0af50d7d0ae59fe81de309f50dbd
Minh Nguyen <tienminh.uet@gmail.com>
2020-10-02 00:54:50 +0900

    ok

--- opencl-ndrange/compile_all.sh
+++ opencl-ndrange/compile_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-aoc device/hifp_4096.cl -o bin_4096/hifp.aocx -board=pac_a10
-echo "4096 done"
-echo "----------------------------"
+# aoc device/hifp_4096.cl -o bin_4096/hifp.aocx -board=pac_a10
+# echo "4096 done"
+# echo "----------------------------"
 
-aoc device/hifp_2048.cl -o bin_2048/hifp.aocx -board=pac_a10
-echo "2048 done"
-echo "----------------------------"
+# aoc device/hifp_2048.cl -o bin_2048/hifp.aocx -board=pac_a10
+# echo "2048 done"
+# echo "----------------------------"
 
-aoc device/hifp_1024.cl -o bin_1024/hifp.aocx -board=pac_a10
-echo "1024 done"
-echo "----------------------------"
+# aoc device/hifp_1024.cl -o bin_1024/hifp.aocx -board=pac_a10
+# echo "1024 done"
+# echo "----------------------------"
 
 # aoc device/hifp_512.cl -o bin_512/hifp.aocx -board=pac_a10
 # echo "512 done"
@@ -29,6 +29,21 @@ echo "----------------------------"
 
 # aoc device/hifp_32.cl -o bin_32/hifp.aocx -board=pac_a10
 # echo "32 done"
+
+aoc device/hifp_16.cl -o bin_16/hifp.aocx -board=pac_a10
+echo "16 done"
+
+aoc device/hifp_8.cl -o bin_8/hifp.aocx -board=pac_a10
+echo "8 done"
+
+aoc device/hifp_4.cl -o bin_4/hifp.aocx -board=pac_a10
+echo "4 done"
+
+aoc device/hifp_2.cl -o bin_2/hifp.aocx -board=pac_a10
+echo "2 done"
+
+aoc device/hifp_1.cl -o bin_1/hifp.aocx -board=pac_a10
+echo "1 done"
 
 echo "============================"
 echo "all done"
---
+++ opencl-ndrange/device/hifp_2.cl
@@ -0,0 +1,48 @@
+__attribute__ ((reqd_work_group_size(2, 1, 1)))
+__kernel void generate_fpid(
+    __global const short int * restrict wave,
+    __global short int * restrict fpid
+)
+{
+    int lid = get_local_id(0);
+    
+    __local short int dwtwave[4097];    
+    
+    int fpid_offset = lid * 2048;
+    
+    // dwt    
+    for (int i=0; i<2048; i++) {
+        int wave_offset = (fpid_offset + i) * 32;
+
+        short int wave_tmp[8];
+        
+        for (int j=0; j<8; j++) {
+            wave_tmp[j] = wave[wave_offset + j];
+        }
+
+        /* 3-stages HAAR wavelet transform */
+        for (int k=8; k>1; k/=2) {
+            for (int l=0; l<k/2; l++) {
+                wave_tmp[l] = (wave_tmp[l*2] + wave_tmp[l*2 + 1]) / 2;
+            }
+        }
+
+        dwtwave[fpid_offset+i] = wave_tmp[0];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+
+    // feature extraction
+    for (int i=0; i<2048; i++) {
+        if (dwtwave[fpid_offset+i] > dwtwave[fpid_offset+i+1]) {
+            fpid[fpid_offset+i] = 1;
+        } else {
+            fpid[fpid_offset+i] = 0;
+        }
+    }
+
+}
+
+
+
